6a215f831df7d46ace994cb28069dc5be63b7531
Dok6n <ehrbs2018@gmail.com>
2022-01-27 20:51:48 +0900

    feat: 수정 게시글 생성 쿼리 현재 시간 저장되도록

--- src/boards/repositories/board.repository.ts
+++ src/boards/repositories/board.repository.ts
@@ -5,7 +5,7 @@ import { EntityRepository, Repository } from 'typeorm';
 @EntityRepository(Board)
 export class BoardRepository extends Repository<Board> {
   async createBoard(board: CreateBoardRequestDto, userId: number, uid: string) {
-    let statement = `INSERT INTO user_board (USER_ID, TITLE, CONTENT, INSERT_ID) VALUE (?, ?, ?, ?)`;
+    let statement = `INSERT INTO user_board (USER_ID, TITLE, CONTENT, INSERT_ID, INSERT_DT) VALUE (?, ?, ?, ?, NOW())`;
     return await this.query(statement, [userId, board.title, board.content, uid]);
   }
 
