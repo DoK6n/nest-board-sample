8e2251eb912812007b451a4964ca1b03f8f61956
Dok6n <ehrbs2018@gmail.com>
2022-02-02 00:56:15 +0900

    refactor: 삭제 board 생성시 INSERT_DT, NOW()
    default값 설정하는 마이그레이션 수행하였으므로 불필요한 작업 제거

--- src/boards/repositories/board.repository.ts
+++ src/boards/repositories/board.repository.ts
@@ -6,7 +6,7 @@ import { EntityRepository, Repository } from 'typeorm';
 @EntityRepository(Board)
 export class BoardRepository extends Repository<Board> {
   async createBoard(board: CreateBoardRequestDto, userId: number, uid: string) {
-    let statement = `INSERT INTO user_board (USER_ID, TITLE, CONTENT, INSERT_ID, INSERT_DT) VALUE (?, ?, ?, ?, NOW())`;
+    let statement = `INSERT INTO user_board (USER_ID, TITLE, CONTENT, INSERT_ID) VALUE (?, ?, ?, ?)`;
     return await this.query(statement, [userId, board.title, board.content, uid]);
   }
 
