a8f6a427b7b64942ab7442c8b207425aef873382
Dok6n <ehrbs2018@gmail.com>
2022-02-01 23:27:41 +0900

    feat: 추가 macOS IP, WiFi 조건

--- src/common/utils/osip.ts
+++ src/common/utils/osip.ts
@@ -1,3 +1,5 @@
+import { config } from 'process';
+
 const os = require('os');
 
 export const getMyIp = (): string => {
@@ -6,11 +8,21 @@ export const getMyIp = (): string => {
   Object.keys(ifaces).forEach(key => {
     for (let i = 0, len = ifaces[key].length; i < len; i++) {
       const details = ifaces[key][i];
-      if ((key === 'Wi-Fi' || key.indexOf('이더넷') !== -1) && details.family === 'IPv4') {
+      if (
+        (key.indexOf('Wi-Fi') !== -1 || // windows
+          key.indexOf('이더넷') !== -1 || // windows
+          key.indexOf('en6') !== -1 || // macOS 이더넷
+          key.indexOf('en0') !== -1) && // macOS Wi-Fi
+        details.family === 'IPv4'
+      ) {
         ip = details.address;
       }
     }
   });
-
-  return ifaces.hasOwnProperty('Wi-Fi') || ifaces.hasOwnProperty('이더넷') ? ip : 'localhost';
+  return ifaces.hasOwnProperty('Wi-Fi') ||
+    ifaces.hasOwnProperty('이더넷') ||
+    ifaces.hasOwnProperty('en6') ||
+    ifaces.hasOwnProperty('en0')
+    ? ip
+    : 'localhost';
 };
